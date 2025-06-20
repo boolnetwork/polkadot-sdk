acd82c6e0024b9f88e684e34767adbfd291c43bc
helin6 <577522102@qq.com>
2025-06-20 14:17:11 +0800

    debug http error

--- primitives/runtime/src/offchain/http.rs
+++ primitives/runtime/src/offchain/http.rs
@@ -233,7 +233,7 @@ impl<'a, I: AsRef<[u8]>, T: IntoIterator<Item = I>> Request<'a, T> {
 }
 
 /// A request error
-#[derive(Clone, PartialEq, Eq, RuntimeDebug)]
+#[derive(Clone, PartialEq, Eq, Debug)]
 pub enum Error {
 	/// Deadline has been reached.
 	DeadlineReached,
